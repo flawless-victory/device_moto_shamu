2992a980f378e0bf2a7cc586ba8b33895afb64bb
Snuzzo <jrracinfan@gmail.com>
2016-10-30 18:35:08 -0400

    Tesla->Deso/Blackbird

--- deso.mk
+++ deso.mk
@@ -3,17 +3,17 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/deso/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := shamu
-PRODUCT_NAME := tesla_shamu
+PRODUCT_NAME := deso_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
